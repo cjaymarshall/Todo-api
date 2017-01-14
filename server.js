a8bc545e9c008deba8c55e9e9cdfb2cc5fa2be67
C Jay Marshall <cjaymarshall1@hotmail.com>
2017-01-14 12:03:13 -0700

    added DELETE to api

--- server.js
+++ server.js
@@ -58,9 +58,23 @@ app.post('/todos', function (req,res) {
 	//push body into array
 	todos.push(body);
 
-	
 	res.json(body);
 });
+
+//DELETE /todos/:id
+
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(!matchedTodo) {
+		res.status(404).json({"error": "No todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+});
+
 	
 
 app.listen(PORT, function () {
