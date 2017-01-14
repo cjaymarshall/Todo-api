cc8972b226a36348a9a8505b274f630b965a5696
C Jay Marshall <cjaymarshall1@hotmail.com>
2017-01-14 11:36:20 -0700

    refactor api with underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -19,13 +21,15 @@ app.get('/todos', function (req,res) {
 app.get('/todos/:id', function (req,res) {
 
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo
-		}
-	});
+	// var matchedTodo;
+
+	// todos.forEach(function (todo) {
+	// 	if (todoId === todo.id) {
+	// 		matchedTodo = todo
+	// 	}
+	// });
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -36,7 +40,18 @@ app.get('/todos/:id', function (req,res) {
 
 //POST /todos
 app.post('/todos', function (req,res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	//pick removes unwanted data
+
+
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length===0) {
+		return res.status(400).send();
+	}
+
+	//set body.description to be trimmed value
+	body.description = body.description.trim();
 
 	//add id field
 	body.id = todoNextId++;
